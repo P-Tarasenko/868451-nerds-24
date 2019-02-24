1bbc333f420b981d86ac8415df5a332bd08cec6f
P-Tarasenko <46486793+P-Tarasenko@users.noreply.github.com>
2019-02-24 18:14:01 +1000

    Исправил замечания

--- js/popup.js
+++ js/popup.js
@@ -44,6 +44,7 @@ link.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
 })
 
 window.addEventListener("keydown", function(evt) {
@@ -51,17 +52,30 @@ window.addEventListener("keydown", function(evt) {
     evt.preventDefault();
     if (popup.classList.contains("modal-show")) {
       popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
     }
   }
 })
 
 form.addEventListener("submit", function (evt) {
-  if (user.value && email.value) {    
-    if (isStorageSupport) {
-      localStorage.setItem("user", user.value);
-    }
+  if (!user.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) { localStorage.setItem("user", user.value); }
     if (isStorageEmailSupport) {
-      localStorage.setItem("email", email.value);
-    }
+    localStorage.setItem("email", email.value); }
   }
-});
+  
+})
+
+// form.addEventListener("submit", function (evt) {
+//   if (user.value && email.value) {    
+//     if (isStorageSupport) {
+//       localStorage.setItem("user", user.value);
+//     }
+//     if (isStorageEmailSupport) {
+//       localStorage.setItem("email", email.value);
+//     }
+//   }
+// });
